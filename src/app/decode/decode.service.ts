28e038d2dfc23a46c6ee99d97c51a290c1d6cfb2
Serhii <sokol.serhij@gmail.com>
2018-12-03 01:30:39 +0300

    Add files for heroku

--- src/app/decode/decode.service.ts
+++ src/app/decode/decode.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Decode } from './decode';
@@ -12,10 +12,12 @@ const httpOptions = {
 
 @Injectable()
 export class DecodeService {
-  // url = 'https://zlata-backend.herokuapp.com/decode';
-  url = 'http://localhost:8080/decode';
+  url = 'https://zlata-backend.herokuapp.com/decode';
   constructor(private http: HttpClient) { }
   code(word: String, maxRes: number): Observable<Decode> {
+    const params = new HttpParams();
+    params.append('max_res', maxRes.toString());
+    httpOptions['params'] = params;
     return this.http.post<Decode>(this.url, JSON.stringify({word: word}), httpOptions);
   }
 }
