83f72b8cf32e04369484a1833a98314796eec6b5
Serhii <sokol.serhij@gmail.com>
2018-12-03 02:11:21 +0300

    hgggg

--- src/app/decode/decode.service.ts
+++ src/app/decode/decode.service.ts
@@ -4,20 +4,17 @@ import { HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Decode } from './decode';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type':  'application/json'
-  })
-};
-
 @Injectable()
 export class DecodeService {
   url = 'https://zlata-backend.herokuapp.com/decode';
   constructor(private http: HttpClient) { }
   code(word: String, maxRes: number): Observable<Decode> {
-    const params = new HttpParams();
-    params.append('max_res', maxRes.toString());
-    httpOptions['params'] = params;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      }),
+      params: new HttpParams().append('max_res', maxRes.toString())
+    };
     return this.http.post<Decode>(this.url, JSON.stringify({word: word}), httpOptions);
   }
 }
